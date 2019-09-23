4e001e84b5a7974f3a0b1b16998d8ebc3295d148
Paco Nieto <50113671+CodeNextPaco@users.noreply.github.com>
2019-09-23 09:10:20 -0700

    Added files

---
+++ Credentials.h
@@ -0,0 +1,31 @@
+/*
+Copyright 2017 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+*/
+
+/* WiFi Network must be 2.4GHz */
+const String SSID = "casa"; 
+const String Password = "balance2017";
+
+/* Your Signal ID should be lowercase, and each word should be separated by dashes. 
+If Google Assistant spoke too quickly, you can ask to repeat the identifier at any 
+time be saying "What is the setup code for [SIGNAL NAME]?" */
+const String SignalID = "successfully-grizzly-tapir";
+
+/* Enter the below API keys to track the weather on Umbrella Signal and Pants Signal. */
+const String DarkSkyAPIKey = "22182c113fe89287fbe116c52c3dce34";
+const String GeoCodingAPiKey = "AIzaSyAJkE6g_p3sJRFMPxKdvNF_DGrhT0AwGjA";
+
+/* Enter the below API key to track stocks on Arrow Signal. */
+const String BarChartAPIKey = "22182c113fe89287fbe116c52c3dce34";
